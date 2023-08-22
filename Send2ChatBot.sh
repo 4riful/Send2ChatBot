adcd88b0963c8dedf3644e5e2801e300578fc725
Ariful Anik <45453772+4riful@users.noreply.github.com>
2023-08-22 20:42:11 +0800

    Update Send2ChatBot.sh

--- Send2ChatBot.sh
+++ Send2ChatBot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONFIG_FILE="config.yaml"
+DEFAULT_CONFIG_FILE="config.yaml"
 MAX_DISCORD_FILE_SIZE=25000000
 LOADING_CHARS="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"  # Characters for loading animation
 HOSTNAME=$(hostname)  # Get the hostname of the system
@@ -12,6 +12,8 @@ DRACULA_ORANGE="\e[0;33m"
 DRACULA_GREEN="\e[0;32m"
 DRACULA_RED="\e[0;31m"
 DRACULA_RESET="\e[0m"
+
+CONFIG_FILE="$DEFAULT_CONFIG_FILE"  # Default configuration file
 
 function send_message_to_telegram() {
     local file_path="$1"
@@ -68,37 +70,60 @@ function send_file_to_discord() {
         sleep 0.1
         echo -ne "\b"
     done
-    echo -e "\b${DRACULA_GREEN}✅ Done!${DRACULA_RESET}"
 
     # Actual Discord sending logic using curl
-    response=$(curl -s -F file=@"$file_path" "$DISCORD_WEBHOOK_URL")
+    response=$(curl -s -o /dev/null -w "%{http_code}" -F file=@"$file_path" "$DISCORD_WEBHOOK_URL")
 
-    if [[ "$response" == *"\"message\":\"Unknown Webhook\""* ]]; then
-        echo -e "${DRACULA_RED}❌ Error:${DRACULA_RESET} Failed to send file to Discord. Webhook is not valid."
-    elif [[ "$response" == *"\"message\":\"payload too large\""* ]]; then
-        echo -e "${DRACULA_RED}❌ Error:${DRACULA_RESET} File size is too large. Discord has a file size limit of approximately 25 MB."
+    if [ "$response" -ne 200 ]; then
+        echo -e "\b${DRACULA_RED}❌ Error:${DRACULA_RESET} Failed to send file to Discord. Response code: $response"
     else
-        echo -e "${DRACULA_GREEN}✅ Success:${DRACULA_RESET} File sent successfully to Discord from $HOSTNAME: $file_name"
+        echo -e "\b${DRACULA_GREEN}✅ Success:${DRACULA_RESET} File sent successfully to Discord from $HOSTNAME: $file_name"
     fi
 }
 
 function print_usage() {
-    echo "Usage: $0 [-t | -d] <file_path>"
+    echo "Usage: $0 [-t | -d] [-c config_file] <file_path>"
     echo "Options:"
-    echo "  -t  Send the file to Telegram"
-    echo "  -d  Send the file to Discord"
+    echo "  -t              Send the file to Telegram"
+    echo "  -d              Send the file to Discord"
+    echo "  -c config_file  Specify a different configuration file (default: config.yaml)"
 }
+
+# Parse command line arguments
+while getopts ":tdc:" opt; do
+    case $opt in
+        t)
+            action="telegram"
+            ;;
+        d)
+            action="discord"
+            ;;
+        c)
+            CONFIG_FILE="$OPTARG"
+            ;;
+        \?)
+            echo -e "${DRACULA_ORANGE}❌ Error:${DRACULA_RESET} Invalid option: -$OPTARG"
+            print_usage
+            exit 1
+            ;;
+        :)
+            echo -e "${DRACULA_ORANGE}❌ Error:${DRACULA_RESET} Option -$OPTARG requires an argument."
+            print_usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 # Customize the output messages with colors
 echo -e "${DRACULA_PURPLE}🔗 Send2ChatBot: Share files effortlessly${DRACULA_RESET}"
 echo -e "${DRACULA_CYAN}------------------------------------------------${DRACULA_RESET}"
 
 # Add alias to use the script globally
-
 echo -e "${DRACULA_ORANGE}🌟 Tip:${DRACULA_RESET} To use this script globally, you can create an alias like:"
 echo -e "${DRACULA_CYAN}    alias s2cb='$PWD/Send2ChatBot.sh'${DRACULA_RESET}"
 
-if [ -z "$1" ]; then
+if [ -z "$action" ]; then
     echo -e "${DRACULA_ORANGE}❌ Error:${DRACULA_RESET} No flag specified."
     print_usage
     exit 1
@@ -109,28 +134,13 @@ if [ ! -f "$CONFIG_FILE" ]; then
     exit 1
 fi
 
-case $1 in
-    -t)
+case "$action" in
+    telegram)
         echo -e "${DRACULA_CYAN}📤 Sending to Telegram...${DRACULA_RESET}"
-        send_message_to_telegram "$2"
+        send_message_to_telegram "$1"
         ;;
-    -d)
+    discord)
         echo -e "${DRACULA_CYAN}📤 Sending to Discord...${DRACULA_RESET}"
-        send_file_to_discord "$2"
-        ;;
-    *)
-        # Check if both configurations are available
-        TELEGRAM_API_KEY=$(grep 'api_key' $CONFIG_FILE | awk '{print $2}' | sed 's/"//g')
-        DISCORD_WEBHOOK_URL=$(grep 'webhook_url' $CONFIG_FILE | awk '{print $2}' | sed 's/"//g')
-        if [ -n "$TELEGRAM_API_KEY" ] && [ -n "$DISCORD_WEBHOOK_URL" ]; then
-            echo -e "${DRACULA_ORANGE}❓ Info:${DRACULA_RESET} No flag specified. Sending to both Telegram and Discord..."
-            send_message_to_telegram "$1"
-            send_file_to_discord "$1"
-        else
-            echo -e "${DRACULA_ORANGE}❌ Error:${DRACULA_RESET} No valid configuration found for Telegram or Discord."
-            print_usage
-            exit 1
-        fi
+        send_file_to_discord "$1"
         ;;
 esac
-
